44e7fcb785410dfed39c3ac1a378c6cf0d9691d8
Chris McKnight <tenlongfingers10@gmail.com>
2023-02-21 15:19:50 -0700

    TaskCard is now a perpetual input form

--- high-stim-todo-app-v2/src/components/TaskCard/EditBtn.js
+++ high-stim-todo-app-v2/src/components/TaskCard/EditBtn.js
@@ -1,49 +1,14 @@
 import React, { useState } from "react";
 
-function EditBtn(props) {
-  const [editing, setEditing] = useState(false);
-  const [inputValue, setInputValue] = useState(props.value);
-
-  const handleEdit = () => {
-    setEditing(true);
+const EditBtn = (props) => {
+  const handleEditBtnClick = () => {
+    props.onClick(props.index);
   };
 
-  const handleTaskInputChange = (e) => {
-    setInputValue(e.target.value);
-  };
-
-  const handleSubmit = () => {
-    props.onEdit(props.index, inputValue);
-    setEditing(false);
-  };
-
-  //JSX
-  if (editing) {
-    return (
-      <li>
-        <input
-          type="text"
-          value={inputValue}
-          onChange={handleTaskInputChange}
-        />
-        <button className="done-edit-btn" onSubmit={handleSubmit}>
-          Done
-        </button>
-      </li>
-    );
-  } else {
-    return (
-      <button className="edit-btn" onClick={handleEdit}>
-        <i class="fa-solid fa-pencil" />
-      </button>
-    );
-  }
-
-  // return (
-  //   <button className="Edit-btn">
-  //     <i class="fa-solid fa-pencil" />
-  //   </button>
-  // );
-}
+  return;
+  <button className="edit-btn" onClick={handleEditBtnClick}>
+    edit
+  </button>;
+};
 
 export default EditBtn;
--- high-stim-todo-app-v2/src/components/TaskCard/TaskCard.js
+++ high-stim-todo-app-v2/src/components/TaskCard/TaskCard.js
@@ -1,17 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 import DeleteBtn from "./DeleteBtn";
 import EditBtn from "./EditBtn";
 import Checkbox from "./Checkbox";
 
 function TaskCard(props) {
-  return (
-    <li className="task-card">
-      <Checkbox />
-      {props.value}
-      <EditBtn />
-      <DeleteBtn />
-    </li>
-  );
+  //for the edit button
+  const [editing, setEditing] = useState(props.value);
+  const [inputValue, setInputValue] = useState(props.value);
+
+  const handleEditBtnClick = () => {
+    setEditing(true);
+  };
+
+  const handleTaskInputChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  //For the submit button when user finishes editing
+  const handleSubmit = () => {
+    props.onEdit(props.index, inputValue);
+    setEditing(false);
+  };
+
+  //JSX
+  if (editing) {
+    return (
+      <li>
+        <input
+          type="text"
+          value={inputValue}
+          onChange={handleTaskInputChange}
+        />
+        <button onClick={handleSubmit}> Done </button>
+      </li>
+    );
+  } else {
+    return (
+      <li>
+        {props.value}
+        <EditBtn onClick={handleEditBtnClick} index={props.index} />
+      </li>
+    );
+  }
+
+  //   return (
+  //     <li className="task-card">
+  //       <Checkbox />
+  //       {props.value}
+  //       <EditBtn />
+  //       <DeleteBtn />
+  //     </li>
+  //   );
 }
 
 export default TaskCard;
